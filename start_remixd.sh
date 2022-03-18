4ab9e48caf1ffb5a1a83f16c288bc87bed250f6f
nischit <nischitpra@gmail.com>
2022-03-18 14:09:13 +0545

    init truffle repo

---
+++ start_remixd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+LOCAL_DIR=$(pwd)
+REMIX_IDE="https://remix.ethereum.org/#optimize=false&runs=200&evmVersion=null&version=soljson-v0.8.7+commit.e28d00a7.js"
+
+tmux new-session -d -s "remixd"
+tmux send-keys -t "remixd" "remixd -s $LOCAL_DIR --remix-ide $REMIX_IDE" Enter
+
+sleep 1s
+
+tmux new-session -d -s "browserIde"
+tmux send-keys -t "browserIde" "xdg-open $REMIX_IDE" Enter
