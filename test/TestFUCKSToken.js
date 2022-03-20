f63319a3df1545a5346163bbe283494a5d920507
nischit <nischitpra@gmail.com>
2022-03-20 15:46:00 +0545

    test cases for token

---
+++ test/TestFUCKSToken.js
@@ -0,0 +1,30 @@
+const FUCKSToken = artifacts.require("FUCKSToken");
+
+contract("test_FUCKSToken", async (accounts) => {
+  it("initial tokens should be in 0th account", async () => {
+    const fucks = await FUCKSToken.deployed();
+    const balance = (await fucks.balanceOf(accounts[0])).toNumber();
+    assert.equal(balance, 2100000000);
+  });
+
+  it("send token from 0th to 1st account", async () => {
+    const fucks = await FUCKSToken.deployed();
+    const tokens = 100000;
+
+    const acc0_balance = (await fucks.balanceOf(accounts[0])).toNumber();
+    const acc1_balance = (await fucks.balanceOf(accounts[1])).toNumber();
+
+    const res = await fucks.transfer(accounts[1], tokens, { from: accounts[0] });
+    console.log("isSuccess", res.receipt.status);
+    assert.equal(res.receipt.status, true, "could not transfer tokens");
+
+    const acc0_balance_new = (await fucks.balanceOf(accounts[0])).toNumber();
+    const acc1_balance_new = (await fucks.balanceOf(accounts[1])).toNumber();
+
+    console.log("0", acc0_balance - tokens, acc0_balance, acc0_balance_new);
+    console.log("1", acc1_balance + tokens, acc1_balance, acc1_balance_new);
+
+    assert.equal(acc0_balance_new, acc0_balance - tokens, "acc 0 balance is not correctly deducted");
+    assert.equal(acc1_balance_new, acc1_balance + tokens, "acc 1 balance is not correctly added");
+  });
+});
